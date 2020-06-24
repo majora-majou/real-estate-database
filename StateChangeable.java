2de94dd6bc2c7524f72d4a9203d4a3b8caa3a867
majora-majou <56742442+majora-majou@users.noreply.github.com>
2020-06-24 07:38:05 -0400

    Update StateChangeable.java

--- StateChangeable.java
+++ StateChangeable.java
@@ -1,10 +1,3 @@
-/**
- * File: StateChangeable.java
- * Author: Jared Buehner
- * Date: May 1, 2020
- * Purpose: This generic interface declares enumerated types as a bounded type parameter and contains an abstract method
- * to change state.
- */
 public interface StateChangeable <T extends Enum<T>> {
     void changeState(T t);
 }
